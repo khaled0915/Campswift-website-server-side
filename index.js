aae09fc2bbe0336e847c7b214e1bd12b6941a270
khaled0915 <khaledsaifullah50956@gmail.com>
2023-11-28 02:13:12 +0600

    implemented verifytoken

--- index.js
+++ index.js
@@ -78,6 +78,23 @@ async function run() {
         return res.status( 401 ).send({message : 'unauthorized access'})
       }
 
+      const token  = req.headers.authorization.split(' ')[1];
+
+      jwt.verify(token , process.env.ACCESS_TOKEN_SECRET , (err , decoded )=>{
+
+        if(err){
+          return res.status(401).send({message : 'unauthorized access' });
+
+
+        }
+
+        req.decoded = decoded ;
+        next();
+
+      })
+
+
+
     }
 
 
