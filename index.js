9b5dcd1800fad1aaf118003bddd2db431163863f
khaled0915 <khaledsaifullah50956@gmail.com>
2023-11-28 01:41:44 +0600

    apply post, patch and delete operation in user related api

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jwxnn.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -41,6 +41,16 @@ async function run() {
 
     const reviewsCollection = client.db('medicalCampDb').collection("feedbacks");
 
+    // user collection 
+
+    const userCollection = client.db('medicalCampDb').collection("users");
+
+    // reg collection 
+
+    const regCollection = client.db('medicalCampDb').collection('reg')
+
+
+
     // get the popular camp 
 
     app.get('/camp' , async(req , res)=>{
@@ -61,6 +71,65 @@ async function run() {
         const result = await reviewsCollection.find().toArray();
         res.send(result);
     })
+
+
+    // user related api 
+
+
+    app.get('/users' , async(req,res)=>{
+      const result = await userCollection.find().toArray();
+
+      res.send(result);
+    })
+
+    app.post('/users' , async(req,res)=>{
+      const user = req.body;
+
+      const query = { email : user.email  }
+
+      const existingUser = await userCollection.findOne(query);
+
+      if(existingUser){
+        return res.send({ message : 'user already exists' , insertedId : null })
+      }
+
+
+      const result = await userCollection.insertOne(user);
+
+      res.send(result);
+    })
+
+
+    app.delete( '/users/:id' , async(req ,res) =>{
+      const id = req.params.id ;
+
+      const query = { _id  : new ObjectId(id)} ;
+
+      const result = await userCollection.deleteOne(query);
+
+      res.send(result);
+    } )
+
+
+    app.patch( '/users/admin/:id' , async(req, res)=>{
+
+      const id = req.params.id ;
+      const filter = { _id : new ObjectId(id) };
+
+      const updatedDoc = {
+        $set : {
+          role : 'organizer'
+        }
+
+      }
+
+
+      const result = await userCollection.updateOne(filter , updatedDoc);
+
+      res.send(result)
+    } )
+
+
 
 
 
