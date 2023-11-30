ac696b1f22b88aded1556d55a2ee8e0826435f81
khaled0915 <khaledsaifullah50956@gmail.com>
2023-11-30 17:09:03 +0600

    add verify token and verify organizer to  get method

--- index.js
+++ index.js
@@ -230,6 +230,16 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/users/:id' , verifyToken, verifyOrganizer , async(req,res)=>{
+      const id =req.params.id; 
+      
+
+      const query = { _id  : new ObjectId(id)} ;
+
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    })
+
     app.post('/users' , async(req,res)=>{
       const user = req.body;
 
